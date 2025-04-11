4c516f112345953d43ca1337ff396f633f0ab7f0
k023g0096@g.neec.ac.jp <k023g0096@g.neec.ac.jp>
2025-04-11 10:39:17 +0900

    青い画面の表示

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,7 +1,50 @@
 #include <Windows.h>
+#include "KamataEngine.h"
+#include "GameScene.h"
+
+using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+	//DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 	
+	//ゲームシーンのインスタンス生成
+	GameScene* gameScene = new GameScene();
+	//ゲームシーンの初期化
+	gameScene->Initialize();
+
+	//エンジンの初期化
+	KamataEngine::Initialize(L"LE3D_05_カラサワ_ミクム_CG4");
+	
+	//メインループ
+	while (true) {
+		//エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		// ゲームシーンの更新
+		gameScene->Update();
+
+		// 描画開始
+		dxCommon->PreDraw();
+
+		// ゲームシーンの描画
+		gameScene->Draw();
+
+		// 描画終了
+		dxCommon->PostDraw();
+	}
+
+	//ゲームシーンの解放
+	delete gameScene;
+
+	//nullptrの代入
+	gameScene = nullptr;
+
+	//更新の終了処理
+	KamataEngine::Finalize();
+
 	return 0;
 }
