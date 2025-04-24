456ba83b82e01177892a15f3ade73454196e7d48
k023g0096@g.neec.ac.jp <k023g0096@g.neec.ac.jp>
2025-04-24 10:37:59 +0900

    完了

--- DirectXGame/Particle.cpp
+++ DirectXGame/Particle.cpp
@@ -1,5 +1,7 @@
 #include "Particle.h"
 
+using namespace MathUtility;
+
 void Particle::Initialize(Model* model) { 
 	assert(model); 
 	model_ = model;
@@ -10,12 +12,20 @@ void Particle::Initialize(Model* model) {
 
 }
 
-void Particle::Update() { 
-	worldTransform_.TransferMatrix(); 
+void Particle::Update() {
+	worldTransform_.TransferMatrix();
+
+	objectColor_.SetColor(color_);
+
+	worldTransform_.translation_ += {0.0f, 0.1f, 0.0f};
+
+	worldTransform_.TransferMatrix();
+
+	worldTransform_.UpdateMatrix();
 
 }
 
 void Particle::Draw(Camera& camera) { 
-	model_->Draw(worldTransform_, camera);
+	model_->Draw(worldTransform_, camera,&objectColor_);
 
 }
